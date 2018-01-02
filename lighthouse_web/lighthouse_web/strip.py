1f70e211376c82b9b02e3d4a77be428118110805
michaelkrueger <freddy1234@gmx.de>
2018-01-02 18:55:16 +0100

    typo

--- lighthouse_web/lighthouse_web/strip.py
+++ lighthouse_web/lighthouse_web/strip.py
@@ -38,23 +38,23 @@ class Strip:
 	
   def test():
     print ('Theater chase animations.')
-    theaterChase(strip, Color(127, 127, 127))  # White theater chase
-    theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-    theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
+    theaterChase(self.strip, Color(127, 127, 127))  # White theater chase
+    theaterChase(self.strip, Color(127,   0,   0))  # Red theater chase
+    theaterChase(self.strip, Color(  0,   0, 127))  # Blue theater chase
     print ('Rainbow animations.')
-    rainbow(strip)
-    rainbowCycle(strip)
-    theaterChaseRainbow(strip)
+    rainbow(self.strip)
+    rainbowCycle(self.strip)
+    theaterChaseRainbow(self.strip)
 	
   # Define functions which animate LEDs in various ways.
-  def colorWipe(color, range = range(strip.numPixels()), wait_ms=50):
+  def colorWipe(color, range = range(self.strip.numPixels()), wait_ms=50):
     """Wipe color across display a pixel at a time."""
     for i in range:
         self.strip.setPixelColor(i, color)
         self.strip.show()
         time.sleep(wait_ms/1000.0)
 
-  def theaterChase(strip, color, wait_ms=50, iterations=10):
+  def theaterChase(color, wait_ms=50, iterations=10):
     """Movie theater light style chaser animation."""
     for j in range(iterations):
         for q in range(3):
@@ -76,29 +76,29 @@ class Strip:
         pos -= 170
         return Color(0, pos * 3, 255 - pos * 3)
 
-  def rainbow(strip, wait_ms=20, iterations=1):
+  def rainbow(wait_ms=20, iterations=1):
     """Draw rainbow that fades across all pixels at once."""
     for j in range(256*iterations):
-        for i in range(strip.numPixels()):
+        for i in range(self.strip.numPixels()):
             self.strip.setPixelColor(i, wheel((i+j) & 255))
         self.strip.show()
         time.sleep(wait_ms/1000.0)
 
-  def rainbowCycle(strip, wait_ms=20, iterations=5):
+  def rainbowCycle(wait_ms=20, iterations=5):
     """Draw rainbow that uniformly distributes itself across all pixels."""
     for j in range(256*iterations):
-        for i in range(strip.numPixels()):
-            self.strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        for i in range(self.strip.numPixels()):
+            self.strip.setPixelColor(i, wheel((int(i * 256 / self.strip.numPixels()) + j) & 255))
         self.strip.show()
         time.sleep(wait_ms/1000.0)
 
-  def theaterChaseRainbow(strip, wait_ms=50):
+  def theaterChaseRainbow(wait_ms=50):
     """Rainbow movie theater light style chaser animation."""
     for j in range(256):
         for q in range(3):
-            for i in range(0, strip.numPixels(), 3):
+            for i in range(0, self.strip.numPixels(), 3):
                 self.strip.setPixelColor(i+q, wheel((i+j) % 255))
             self.strip.show()
             time.sleep(wait_ms/1000.0)
-            for i in range(0, strip.numPixels(), 3):
+            for i in range(0, self.strip.numPixels(), 3):
                 self.strip.setPixelColor(i+q, 0)        
