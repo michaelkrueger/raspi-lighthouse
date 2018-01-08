c1629ede5aa2e4a9daf49fc8e983886b037f8974
michaelkrueger <freddy1234@gmx.de>
2018-01-08 20:49:39 +0100

    setup.py

--- lighthouse_web/setup.py
+++ lighthouse_web/setup.py
@@ -7,4 +7,8 @@ setup(
     install_requires=[
         'flask',
     ],
+	ext_modules  = [Extension('_rpi_ws281x', 
+                                     sources=['rpi_ws281x.i'],
+                                     library_dirs=['../../rpi_ws281x'],
+                                     libraries=['ws2811', 'rt'])])
 )
